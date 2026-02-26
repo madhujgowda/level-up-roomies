fb85d596b29b582fbabe0499fd7935c656cb2cda
Madhu Jayarama <madhu.gowda.j@gmail.com>
2026-02-25 21:13:53 -0600

    git ignore and environment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,27 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-
+import { environment } from '../environments/environment';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-
-
-const firebaseConfig = {
-  apiKey: "AIzaSyC7sQdkesqEQvl3e2KY8khUe2eDPF-xNGU",
-  authDomain: "level-up-roomies.firebaseapp.com",
-  projectId: "level-up-roomies",
-  storageBucket: "level-up-roomies.firebasestorage.app",
-  messagingSenderId: "655924300985",
-  appId: "1:655924300985:web:790bd8af923e19e290df68"
-};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore())
   ]
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "",
+    authDomain: "l",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  }
+};
